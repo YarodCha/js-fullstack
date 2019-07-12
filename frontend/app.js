a51a432f867fb47442cc4cb10d3934937f797edd
YarodCha <yarod_12@hotmail.com>
2019-07-12 14:56:32 -0500

    First commit

---
+++ frontend/app.js
@@ -0,0 +1,33 @@
+import "./styles/app.css";
+import UI from "./UI";
+
+const ui = new UI();
+
+document.addEventListener("DOMContentLoaded", () => {
+  ui.renderBooks();
+});
+
+document.getElementById("book-form").addEventListener("submit", e => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const isbn = document.getElementById("isbn").value;
+  const image = document.getElementById("image").files;
+
+  const formData = new FormData();
+  formData.append("image", image[0]);
+  formData.append("title", title);
+  formData.append("author", author);
+  formData.append("isbn", isbn);
+
+  ui.addANewBook(formData);
+  ui.renderMessage('New Book Added','success',3000)
+  e.preventDefault();
+});
+
+document.getElementById("books-cards").addEventListener("click", e => {
+  if (e.target.classList.contains('delete')) {
+      ui.deleteBook(e.target.getAttribute('_id'))
+      ui.renderMessage('Book deleted','danger',2000)
+  }
+  e.preventDefault()
+});
